89e1b0a5f5d1b023cb9be4b3f622afe8df4a3805
Rahman Yusuf <mansuf@mansuf.link>
2023-02-12 13:43:50 +0700

    FIx DNS Cache

--- requests_doh/cachemanager.py
+++ requests_doh/cachemanager.py
@@ -11,10 +11,10 @@ class DNSCacheManager:
         else:
             raise ValueError(f'{time.__class__.__name__} is not float type')
     
-    def set_cache(self, host, answers, provider):
+    def set_cache(self, host, answers):
         self._data[host] = {
             "expire": datetime.now() + self._expire,
-            "data": [answers, provider]
+            "data": answers
         }
     
     def get_cache(self, host):
