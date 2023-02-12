6cf75504f9f23a20861c67416a2fb64333d905fa
Rahman Yusuf <mansuf@mansuf.link>
2023-02-12 22:05:50 +0700

    bump version 0.3.0

--- requests_doh/__init__.py
+++ requests_doh/__init__.py
@@ -2,7 +2,7 @@
 DNS over HTTPS resolver for python requests
 """
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 __description__ = "DNS over HTTPS resolver for python requests"
 __author__ = "Rahman Yusuf"
 __author_email__ = "danipart4@gmail.com"
